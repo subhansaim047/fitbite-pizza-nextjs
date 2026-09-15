7a1237bef654700bdffcd8b56edad51003d1da44
subhansaim047 <subhansaim047@gmail.com>
2026-09-15 22:32:59 +0300

    fix: restore clean layout and remove middleware

--- middleware.ts
+++
@@ -1,6 +0,0 @@
-﻿import { NextResponse } from 'next/server';
-export function middleware(request) {
-  if (request.nextUrl.pathname === '/') {
-    return NextResponse.rewrite(new URL('/fitbitepizza/index.html', request.url));
-  }
-}
