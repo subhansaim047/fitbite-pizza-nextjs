6eb0d95e9ec13f387c519a1eb53a4f19bcb5fd5f
subhansaim047 <subhansaim047@gmail.com>
2026-09-15 22:31:23 +0300

    fix: resolve utf-16 encoding bug in next config and middleware

---
+++ middleware.ts
@@ -0,0 +1,6 @@
+﻿import { NextResponse } from 'next/server';
+export function middleware(request) {
+  if (request.nextUrl.pathname === '/') {
+    return NextResponse.rewrite(new URL('/fitbitepizza/index.html', request.url));
+  }
+}
